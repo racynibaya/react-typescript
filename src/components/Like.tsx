5b5292a9d5673f7bf1a0d7f78f442f0a0850cc0a
Racyn Ibaya <racynibaya@outlook.com>
2023-03-18 09:15:24 -0400

    Like exercise button

--- src/components/Like.tsx
+++ src/components/Like.tsx
@@ -3,11 +3,16 @@ import { MouseEvent, useState } from 'react';
 
 import { AiOutlineHeart, AiFillHeart } from 'react-icons/ai';
 
-const Like = () => {
+interface Props {
+  onClick: () => void;
+}
+
+const Like = ({ onClick }: Props): JSX.Element => {
   const [isClicked, setIsClicked] = useState(false);
 
   const handleClick = (e: MouseEvent) => {
     setIsClicked(!isClicked);
+    onClick();
   };
   return (
     <>
