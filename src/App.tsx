02602f4ab17a8f39787888e860dc6d52948ef5be
Racyn Ibaya <racynibaya@outlook.com>
2023-03-18 11:14:20 -0400

    Exercise to update a state with array of objects

--- src/App.tsx
+++ src/App.tsx
@@ -6,25 +6,32 @@ import Alert from './components/Alert';
 import Like from './components/Like';
 
 const App = () => {
-  const [pizza, setPizza] = useState({
-    name: 'Spicy Pepperoni',
-    toppings: ['Mushroom'],
+  const [cart, setCart] = useState({
+    discount: 0.1,
+    items: [
+      { id: 1, title: 'Product 1', quantity: 1 },
+      { id: 2, title: 'Product 2', quantity: 1 },
+    ],
   });
-
+  console.log(cart.items[0].quantity);
   const handleClick = () => {
-    setPizza({
-      ...pizza,
-      toppings: [...pizza.toppings, 'Makdo'],
+    setCart({
+      ...cart,
+      items: cart.items.map(item =>
+        item.id === 1 ? { ...item, quantity: (item.quantity += 1) } : item
+      ),
     });
   };
 
-  console.log(pizza);
-
   return (
     <div className='App'>
-      <p>{pizza.name}</p>
-      <p>Ingredients: {...pizza.toppings}</p>
-      <button onClick={handleClick}>Add toppings</button>
+      {/* <p>{pizza.name}</p>
+      <p>Ingredients: {...pizza.toppings}</p> */}
+      {cart.items.map((item, i) => (
+        <p key={item.id}>{item.quantity}</p>
+      ))}
+
+      <button onClick={handleClick}>Add count</button>
     </div>
   );
 };
