305db7ca1e28f29c337ac0d0b82b264cb6154249
Racyn Ibaya <racynibaya@outlook.com>
2023-03-18 09:28:02 -0400

    adding pizza toppings

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,25 @@ import Alert from './components/Alert';
 import Like from './components/Like';
 
 const App = () => {
-  const items = ['New york', 'San Franciso', 'Tokyo', 'London'];
-  const [clicked, setClicked] = useState(false);
-  console.log(clicked);
+  const [pizza, setPizza] = useState({
+    name: 'Spicy Pepperoni',
+    toppings: ['Mushroom'],
+  });
+
+  const handleClick = () => {
+    setPizza({
+      ...pizza,
+      toppings: [...pizza.toppings, 'Makdo'],
+    });
+  };
+
+  console.log(pizza);
 
   return (
     <div className='App'>
-      <Like />
+      <p>{pizza.name}</p>
+      <p>Ingredients: {...pizza.toppings}</p>
+      <button onClick={handleClick}>Add toppings</button>
     </div>
   );
 };
