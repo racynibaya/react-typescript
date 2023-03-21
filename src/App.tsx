615ca73b7afc3d74cae7c0f17156154abd9c4f4e
Racyn Ibaya <racynibaya@outlook.com>
2023-03-21 13:34:37 -0400

    created a seperate file for categories, ExpenseForm is created as well as the form validation, accessing the form field, filtering all of the expenses

--- src/App.tsx
+++ src/App.tsx
@@ -12,23 +12,38 @@ import {
   Message,
   User,
 } from './components';
+import ExpenseFilter from './expense-tracker/components/ExpenseFilter';
+import ExpenseForm from './expense-tracker/components/ExpenseForm';
+
+
 
 const App = () => {
+  const [selectedCategory, setSelectedCategory] = useState('');
   const [expenses, setExpenses] = useState([
     { id: 1, description: 'aaa', amount: 10, category: 'Utilities' },
     { id: 2, description: 'bbb', amount: 10, category: 'Utilities' },
-    { id: 3, description: 'ccc', amount: 10, category: 'Utilities' },
-    { id: 4, description: 'ddd', amount: 10, category: 'Utilities' },
+    { id: 3, description: 'ccc', amount: 10, category: 'Groceries' },
+    { id: 4, description: 'ddd', amount: 10, category: 'Entertainment' },
   ]);
+
+  const onDelete = (id: number) =>
+    setExpenses(expenses.filter(e => e.id !== id));
+
+  const catFilteringHandler = (category: string) => {
+    setSelectedCategory(category);
+  };
+
+  const visibleExpense = selectedCategory
+    ? expenses.filter(e => e.category === selectedCategory)
+    : expenses;
 
   return (
     <div className='App'>
-      <ExpenseList
-        expenses={expenses}
-        onDelete={id =>
-          setExpenses(expenses.filter(expense => expense.id !== id))
-        }
-      />
+      <div className='mb-3'>
+        <ExpenseForm />
+      </div>
+      <ExpenseList expenses={visibleExpense} onDelete={onDelete} />
+      <ExpenseFilter onSelectCategory={catFilteringHandler} />
     </div>
   );
 };
