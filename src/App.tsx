e0de787752f78f35e9661efd5c3457d16e6697ce
Racyn Ibaya <racynibaya@outlook.com>
2023-03-22 20:49:10 -0400

    Cancelling a fetch request

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,46 @@
 import { useEffect, useState } from 'react';
-import ProductList from './components/ProductList';
+import axios, { AxiosError, CanceledError } from 'axios';
 
 // Simulating connection
 const connect = () => console.log('Connected');
 const disconnect = () => console.log('Disconnected');
 
-const App = () => {
-  useEffect(() => {
-    connect();
+// for better development
+interface User {
+  id: number;
+  name: string;
+  address: object;
+}
 
-    // Effect clean up: It should stop or undo whatever the effect was doing.
-    return () => disconnect();
-  });
-  return <div></div>;
+const App = () => {
+  const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    // Allows us to cancel or abort async operation like fetch request, dom manipulation, any operation that may take time to complete.
+    const controller = new AbortController();
+    const fetchData = async () => {
+      try {
+        const { data } = await axios.get<User[]>(
+          'https://jsonplaceholder.typicode.com/users',
+          { signal: controller.signal }
+        );
+      } catch (err) {
+        if (err instanceof CanceledError) return;
+        setError((err as AxiosError).message);
+      }
+    };
+
+    return controller.abort();
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Hotdog</h1>
+    </div>
+  );
 };
 
 export default App;
