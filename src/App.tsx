2a76e6d4a688de0d8f8df59bb4ee0658b564f40f
Racyn Ibaya <racynibaya@outlook.com>
2023-03-23 14:28:18 -0400

    How to show a loading indicator

--- src/App.tsx
+++ src/App.tsx
@@ -15,29 +15,41 @@ interface User {
 const App = () => {
   const [users, setUsers] = useState<User[]>([]);
   const [error, setError] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     // Allows us to cancel or abort async operation like fetch request, dom manipulation, any operation that may take time to complete.
     const controller = new AbortController();
-    const fetchData = async () => {
-      try {
-        const { data } = await axios.get<User[]>(
-          'https://jsonplaceholder.typicode.com/users',
-          { signal: controller.signal }
-        );
-      } catch (err) {
+    setIsLoading(true);
+    console.log('loading');
+
+    axios
+      .get<User[]>('https://jsonplaceholder.typicode.com/users', {
+        signal: controller.signal,
+      })
+      .then(res => {
+        console.log(res);
+
+        setUsers(res.data);
+        setIsLoading(false);
+      })
+      .catch(err => {
+        console.log(err);
+
         if (err instanceof CanceledError) return;
         setError((err as AxiosError).message);
-      }
-    };
+        setIsLoading(false);
+        console.log('error');
+      });
 
-    return controller.abort();
-
-    fetchData();
+    // return controller.abort();
   }, []);
+
+  console.log(isLoading);
 
   return (
     <div>
+      {isLoading && <div className='spinner-border'></div>}
       <h1>Hotdog</h1>
     </div>
   );
